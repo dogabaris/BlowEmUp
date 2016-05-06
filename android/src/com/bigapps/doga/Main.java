066bf28584350f2692ecb843f2b47e90bf9f1af1
dogabaris <dogabarisozdemir@gmail.com>
2016-05-06 01:25:43 +0000

    init

---
+++ android/src/com/bigapps/doga/Main.java
@@ -0,0 +1,42 @@
+package com.bigapps.doga;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by shadyfade on 06.05.2016.
+ */
+public class Main extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            setContentView(R.layout.activity_oyun);
+
+            Button btn_yeni = (Button) findViewById(R.id.btn_yenioyun);
+            Button btn_ses = (Button) findViewById(R.id.btn_ses);
+            Button btn_cikis = (Button) findViewById(R.id.btn_cikis);
+            Button btn_hakkinda = (Button) findViewById(R.id.btn_hakkinda);
+
+            btn_yeni.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent oyun = new Intent(v.getContext(), Oyun.class);
+                    startActivity(oyun);
+                }
+            });
+        } catch (Exception e) {
+            Log.e("Activate", "Error in activity", e);
+
+            Toast.makeText(getApplicationContext(),
+                    e.getClass().getName() + " " + e.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ android/src/com/bigapps/doga/Oyun.java
@@ -0,0 +1,15 @@
+package com.bigapps.doga;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class Oyun extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BlowEmUp(), config);
+	}
+}
